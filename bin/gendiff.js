081304e22f2621f766dcbef88701cb8946fc7c1b
Ilia Adadurov <adadurovin@yandex.ru>
2021-09-07 21:53:18 +0400

    Step 2 done

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,9 @@ import commander from '../node_modules/commander/index.js';
 const program = new commander.Command();
 
 program
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .option('-f, --format [type]', 'output format');
 
-program.parse();
+program.parse(process.argv);
