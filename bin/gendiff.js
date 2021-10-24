a5d00a94c0f3e451f65f45ab9fd8765b16fa83be
Ilia Adadurov <adadurovin@yandex.ru>
2021-10-25 00:20:49 +0400

    Step 3 results

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 
-import commander from '../node_modules/commander/index.js';
+// import commander from '../node_modules/commander/index.js';
 
-const program = new commander.Command();
+import { program } from 'commander';
+import genDiff from '../index.js';
+
+// const program = new commander.Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<filepath1> <filepath2>')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'output format');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
-program.parse(process.argv);
+program.parse();
--- index.js
+++ index.js
@@ -1,5 +1,53 @@
-const genDiff = () => {
-  const c = [];
+import fs from 'fs';
+import path from 'path';
+
+const genDiff = (filePath1, filePath2) => {
+// find difference between two JSON files
+
+  // const filePath11 = '../__tests__/fixtures/file1.json';
+  const filePath11 = '/home/adadurovin/frontend-project-lvl2/__tests__/fixtures/file1.json';
+  // const filePath22 = '../__tests__/fixtures/file2.json';
+  const filePath22 = '/home/adadurovin/frontend-project-lvl2/__tests__/fixtures/file2.json';
+
+  const firstFileContent = fs.readFileSync(filePath11, { encoding: 'utf8' });
+  const firstJSONParsed = JSON.parse(firstFileContent);
+  const firstObjectKeys = Object.keys(firstJSONParsed);
+
+  const secondFileContent = fs.readFileSync(filePath22, { encoding: 'utf8' });
+  const secondJSONParsed = JSON.parse(secondFileContent);
+  const secondObjectKeys = Object.keys(secondJSONParsed);
+
+  const uniqueKeysNames = Array.from(new Set([...firstObjectKeys, ...secondObjectKeys])).sort();
+
+  const diffRecord = uniqueKeysNames.reduce((acc, key) => {
+    if (key in firstJSONParsed && key in secondJSONParsed) {
+      if (firstJSONParsed[key] === secondJSONParsed[key]) {
+        acc[key] = firstJSONParsed[key];
+        // console.log(acc);
+        return acc;
+      }
+      acc[`- ${key}`] = firstJSONParsed[key];
+      acc[`+ ${key}`] = secondJSONParsed[key];
+      return acc;
+    }
+
+    if (key in firstJSONParsed) {
+      acc[`- ${key}`] = firstJSONParsed[key];
+      return acc;
+    }
+
+    if (key in secondJSONParsed) {
+      acc[`+ ${key}`] = secondJSONParsed[key];
+      return acc;
+    }
+
+    return acc;
+  }, {});
+
+  const diffToString = `{\n${Object.entries(diffRecord).map(([key, value]) => `${key}: ${value}`).join('\n')}\n}`;
+
+  const c = diffToString;
+
   return c;
 };
 
