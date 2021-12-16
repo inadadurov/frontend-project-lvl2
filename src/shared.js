4b077264bc0ffec6f9425a3b6a9d0b37d4cece18
Ilia Adadurov <adadurovin@yandex.ru>
2021-12-16 23:02:12 +0400

    check 1 - fixed 6th

--- src/shared.js
+++ src/shared.js
@@ -1,20 +1,7 @@
 import _ from 'lodash';
 
-const getPropStatusAndValues = (propName, obj) => {
-  if (propName.charAt(0) !== '+' && propName.charAt(0) !== '-') {
-    return ['unchanged', obj[propName], undefined];
-  }
-  if (propName.charAt(0) === '+' || propName.charAt(0) === '-') {
-    const nameOnly = propName.substring(2, propName.length);
-    if (_.has(obj, `+ ${nameOnly}`) && _.has(obj, `- ${nameOnly}`)) return ['updated', obj[`- ${nameOnly}`], obj[`+ ${nameOnly}`]];
-    if (_.has(obj, `+ ${nameOnly}`) && !_.has(obj, `- ${nameOnly}`)) return ['added', undefined, obj[`+ ${nameOnly}`]];
-    return ['removed', obj[`- ${nameOnly}`], undefined];
-  }
-  return 'branch';
-};
-
-const putQuotesIfNeeded = (value) => ([true, false, null, undefined].includes(value) ? value : `'${value}'`);
+const putQuotesIfNeeded = (value) => (([true, false, null, undefined].includes(value) || _.isFinite(value)) ? value : `'${value}'`);
 
 const diffProperties = ['status', 'type', 'typeNew', 'valueOld', 'valueNew'];
 
-export { getPropStatusAndValues, putQuotesIfNeeded, diffProperties };
+export { putQuotesIfNeeded, diffProperties };
